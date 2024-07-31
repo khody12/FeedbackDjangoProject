7ec84a18da7d42b791c8084212c69de7c5ab0ca9
khody <ekhodor8@gmail.com>
2024-07-31 16:58:15 -0700

    added class based views

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path("", views.review),
+    path("", views.ReviewView.as_view()), #as_view() makes the class act as a view.
     path("thank-you",views.thank_you)
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -2,29 +2,49 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 # Create your views here.
 from .forms import ReviewForm
+from django.views import View
 
-def review(request):
+class ReviewView(View): #
+    def get(self,request):
+        form = ReviewForm()
 
-    if request.method == "POST":
-        form = ReviewForm(request.POST)
+        return render(request, "reviews/review.html",{
+        "form":form
+        })
 
-        #if we wanted to update an existing data set in our data base.
-        #existing_data = Review.objects.get(pk=1) # pk is an identifier
-        #form = ReviewForm(request.POST,instance=existing_data)
+    def post(self,request):
         form = ReviewForm(request.POST)
 
         if form.is_valid(): # is valid is coming from the Form class we are inheriting from and it will check that user inputed data is valid. 
             form.save()
             #print(form.cleaned_data) # cleaned_data is a field that contains all of our validated data automatically. This is a field that exists on the form class were inheriting from.
             return HttpResponseRedirect("/thank-you")
-    else:
-        form = ReviewForm()
+
+
+
+
+# def review(request):
+
+#     if request.method == "POST":
+#         form = ReviewForm(request.POST)
+
+#         #if we wanted to update an existing data set in our data base.
+#         #existing_data = Review.objects.get(pk=1) # pk is an identifier
+#         #form = ReviewForm(request.POST,instance=existing_data)
+#         form = ReviewForm(request.POST)
+
+#         if form.is_valid(): # is valid is coming from the Form class we are inheriting from and it will check that user inputed data is valid. 
+#             form.save()
+#             #print(form.cleaned_data) # cleaned_data is a field that contains all of our validated data automatically. This is a field that exists on the form class were inheriting from.
+#             return HttpResponseRedirect("/thank-you")
+#     else:
+#         form = ReviewForm()
 
    
 
-    return render(request, "reviews/review.html",{
-        "form":form
-    })
+#     return render(request, "reviews/review.html",{
+#         "form":form
+#     })
 
 def thank_you(request):
     return render(request,"reviews/thank_you.html")
