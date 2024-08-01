537687c68a64f6038f1aaf151fc306f86309e1ac
khody <ekhodor8@gmail.com>
2024-08-01 11:19:00 -0700

    added new templates and implemented TemplateView class view

--- reviews/urls.py
+++ reviews/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns =[
     path("", views.ReviewView.as_view()), #as_view() makes the class act as a view.
-    path("thank-you",views.thank_you)
+    path("thank-you",views.thank_you.as_view()),
+    path("reviews", views.ReviewsListView.as_view())
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,11 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 # Create your views here.
 from .forms import ReviewForm
 from django.views import View
+from django.views.generic.base import TemplateView # view that can be extended that is more specific and focused to build view classes that render templates. 
+from .models import Review
 
 class ReviewView(View): #
     def get(self,request):
@@ -20,7 +23,22 @@ class ReviewView(View): #
             #print(form.cleaned_data) # cleaned_data is a field that contains all of our validated data automatically. This is a field that exists on the form class were inheriting from.
             return HttpResponseRedirect("/thank-you")
 
+class thank_you(TemplateView):
+    template_name = "reviews/thank_you.html" # this is an expected property name, not some variable simply defined by me. itll automatically take this template and return and render if a get request reaches this view.
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = "This works!"
+        return context
+
+class ReviewsListView(TemplateView):
+    template_name = "reviews/review_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context['reviews'] = reviews
+        return context
 
 
 # def review(request):
@@ -46,8 +64,8 @@ class ReviewView(View): #
 #         "form":form
 #     })
 
-def thank_you(request):
-    return render(request,"reviews/thank_you.html")
+
+
 
 
 
