8e2c836f6969ce0ad17accd5e29d5f2c6de3a36b
khody <ekhodor8@gmail.com>
2024-08-02 14:15:58 -0700

    implemented profile form

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    image = models.FileField(upload_to="images") # by default, this upload to will start in the very root directory of our system. not even in the root directory of the project, we must go to settings.py
+    # and configure this using a new setting called MEDIA_ROOT. It will tell it where to look to store files.
--- profiles/views.py
+++ profiles/views.py
@@ -3,17 +3,34 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .forms import ProfileForm
+from .models import UserProfile
 
+# Create your views here.
+# def store_file(file): # this is unnecessary if we use the ProfileForm as django just does this for us. 
+#     with open("temp/image.png", "wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
 
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        return render(request, "profiles/create_profile.html",{
+            "form":form
+        })
 
     def post(self, request):
-        print(request.FILES['image']) # special property populated by django which gives us access to uploaded files. Files is a dictionary, and we have names within our html that is associated with the input
-        # that name, we called it "image", is how we access that file here. 
-        return HttpResponseRedirect("/profiles")
+        submitted_form = ProfileForm(request.POST, request.FILES)
+        if submitted_form.is_valid():
+            profile = UserProfile(image=request.FILES['user_image'])
+            profile.save()
+
+            #store_file(request.FILES['image']) # special property populated by django which gives us access to uploaded files. Files is a dictionary, and we have names within our html that is associated with the input
+            # that name, we called it "image", is how we access that file here. 
+            return HttpResponseRedirect("/profiles")
+        return render(request, "profiles/create_profile.html", {
+            "form": submitted_form
+        })
 
         
-        pass
+
